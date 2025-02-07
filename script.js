6143eedc2e20d2791ae8cf7816393ce3a8d32c0f
Will Cooley <william.cooley@gmail.com>
2025-02-06 22:05:32 -0500

    index.html complete

---
+++ script.js
@@ -0,0 +1,54 @@
+const btnOpen = document.querySelector('#btnOpen')
+const btnClose = document.querySelector('#btnClose')
+
+const media = window.matchMedia('(width < 40em)')
+const topNavMenu = document.querySelector('.topnav__menu')
+const main = document.querySelector('main')
+const body = document.querySelector('body')
+
+
+function setupTopNav(e){
+    if(e.matches){
+        // topNavMenu.setAttribute('inert', '')
+        topNavMenu.style.transition = 'none'
+    } else {
+        topNavMenu.removeAttribute('inert')
+        closeMobileMenu()
+    }
+}
+
+
+function openMobileMenu(){
+
+    btnOpen.setAttribute('aria-expanded', 'true')
+    // topNavMenu.removeAttribute('inert')
+    topNavMenu.removeAttribute('style')
+    // main.setAttribute('inert', '')
+
+    bodyScrollLockUpgrade.disableBodyScroll(body);
+    // btnClose.focus()
+
+}
+
+function closeMobileMenu(){
+
+    btnOpen.setAttribute('aria-expanded', 'false')
+    // topNavMenu.setAttribute('inert', '')
+    // main.removeAttribute('inert')
+
+    bodyScrollLockUpgrade.enableBodyScroll(body);
+    // btnOpen.focus()
+
+    setTimeout(()=>{
+        topNavMenu.style.transition = 'none'
+    }, 500)
+}
+
+setupTopNav(media);
+
+btnOpen.addEventListener('click', openMobileMenu)
+btnClose.addEventListener('click', closeMobileMenu)
+
+media.addEventListener('change', function (e) {
+    setupTopNav(e);
+})
