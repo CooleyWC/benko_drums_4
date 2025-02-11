7da9f382e8a15fc1ac32ddfcdae2bfcb71194fa5
Will Cooley <william.cooley@gmail.com>
2025-02-10 21:01:57 -0500

    re-built navbar

--- script.js
+++ script.js
@@ -1,54 +1,25 @@
-const btnOpen = document.querySelector('#btnOpen')
-const btnClose = document.querySelector('#btnClose')
-
-const media = window.matchMedia('(width < 40em)')
-const topNavMenu = document.querySelector('.topnav__menu')
-const main = document.querySelector('main')
-const body = document.querySelector('body')
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
 
 
-function setupTopNav(e){
-    if(e.matches){
-        // topNavMenu.setAttribute('inert', '')
-        topNavMenu.style.transition = 'none'
-    } else {
-        topNavMenu.removeAttribute('inert')
-        closeMobileMenu()
+navToggle.addEventListener('click', ()=>{
+    const visibility = primaryNav.getAttribute('data-visible')
+
+    if(visibility === 'false'){
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true)
+    } else if(visibility === 'true') {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false)
     }
-}
-
-
-function openMobileMenu(){
-
-    btnOpen.setAttribute('aria-expanded', 'true')
-    // topNavMenu.removeAttribute('inert')
-    topNavMenu.removeAttribute('style')
-    // main.setAttribute('inert', '')
-
-    bodyScrollLockUpgrade.disableBodyScroll(body);
-    // btnClose.focus()
-
-}
-
-function closeMobileMenu(){
-
-    btnOpen.setAttribute('aria-expanded', 'false')
-    // topNavMenu.setAttribute('inert', '')
-    // main.removeAttribute('inert')
-
-    bodyScrollLockUpgrade.enableBodyScroll(body);
-    // btnOpen.focus()
-
-    setTimeout(()=>{
-        topNavMenu.style.transition = 'none'
-    }, 500)
-}
-
-setupTopNav(media);
-
-btnOpen.addEventListener('click', openMobileMenu)
-btnClose.addEventListener('click', closeMobileMenu)
-
-media.addEventListener('change', function (e) {
-    setupTopNav(e);
 })
+
+// this is for disabling animations while resizing viewport
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
